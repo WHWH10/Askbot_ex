4bc0fb3f7c81470010617ffb927eaa886496a4b1
WHWH10 <eyet010@gmail.com>
2017-09-11 16:47:35 +0900

    functions.py modify : 전부

--- plusfriend/functions.py
+++ plusfriend/functions.py
@@ -1,24 +1,22 @@
+
 import json
 import requests
-
 def melon_search(query):
     params = {
-        'jsoncallback' : '_',
-        'query' : query,
+        'jscallback': '_',
+        'query': query,
     }
-    jsonp_string = requests.get('http://www.melon.com/search/keyword/index.json', params = params).text
+    jsonp_string = requests.get('http://www.melon.com/search/keyword/index.json', params=params).text
     json_string = jsonp_string.replace('_(', '').replace(');', '')
+
     meta = json.loads(json_string)
     messages = []
-
-    if 'SONGCONETENTS' in meta:
+    if 'SONGCONTENTS' in meta:
         for song in meta['SONGCONTENTS']:
-            messages.append('''[{ALBUMNAME}] [{SONGNAME}] by {ARTISTNAME}
-- http://www.melon.com/song/detail.htm?songId={SONGID}'''.format(**song))
+            messages.append('''[{ALBUMNAME}] {SONGNAME} by {ARTISTNAME} - http://www.melon.com/song/detail.htm?songId={SONGID}'''.format(**song))
 
     if messages:
         message = '\n'.join(messages)
     else:
         message = '검색어 "{}"에 대한 노래 검색결과가 없습니다.'.format(query)
-
     return message
